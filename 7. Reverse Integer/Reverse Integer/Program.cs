5935c8d8a181362286b7fa8d128f554fbfe50a4f
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-01-07 21:14:06 +0600

    first commit

---
+++ 7. Reverse Integer/Reverse Integer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Reverse_Integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be reversed: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0, rem;
+            if (x < 10)
+                Console.WriteLine("The number must has at least two digit. Try again!");
+            else
+            {
+                while (x != 0)
+                {
+                    rem = x % 10;
+                    reverse = reverse * 10 + rem;
+                    x /= 10;
+                }
+
+                Console.WriteLine("The reversed number is: " + reverse);
+            }
+        }
+    }
+}
