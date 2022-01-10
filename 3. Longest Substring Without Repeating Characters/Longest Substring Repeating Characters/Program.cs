303006165966fee5b395cde85cc66ad33aa78667
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-01-11 01:23:44 +0600

    first commit

---
+++ 3. Longest Substring Without Repeating Characters/Longest Substring Repeating Characters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Longest_Substring_Repeating_Characters
+{
+    public static class Program
+    {
+        static int longestUniqueSubsttr(string str)
+        {
+            int n = str.Length;
+            int res = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool[] visited = new bool[256];
+                for (int j = i; j < n; j++)
+                {
+                    if (visited[str[j]] == true)
+                        break;
+                    else
+                    {
+                        res = Math.Max(res, j - i + 1);
+                        visited[str[j]] = true;
+                    }
+                }
+
+                visited[str[i]] = false;
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine("The input string is " + str);
+
+            int len = longestUniqueSubsttr(str);
+            Console.WriteLine("The length of the longest " +
+                              "non-repeating character " +
+                              "substring is " + len);
+        }
+    }
+}
