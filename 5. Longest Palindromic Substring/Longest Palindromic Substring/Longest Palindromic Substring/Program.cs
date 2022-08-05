e4c4001549a1cc49a0fb87da2ff3882616dedb55
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-08-05 22:05:15 +0600

    added the solution for longest palindromic substring

---
+++ 5. Longest Palindromic Substring/Longest Palindromic Substring/Longest Palindromic Substring/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Longest_Palindromic_Substring
+{
+    public class Program
+    {
+        public static string LongestPalindrome(string s)
+        {
+            int maxLength = 0, startIndex = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int start = i, end = i;
+                while (end < s.Length - 1 && s[start] == s[end + 1])
+                    end++;
+
+                while (end < s.Length - 1 && start > 0 && s[start - 1] == s[end + 1])
+                {
+                    start--;
+                    end++;
+                }
+                if (maxLength < end - start + 1)
+                {
+                    maxLength = end - start + 1;
+                    startIndex = start;
+                }
+            }
+            return s.Substring(startIndex, maxLength);
+        }
+        static void Main(string[] args)
+        {
+            string s = "bababaad";
+            Console.Write(LongestPalindrome(s));
+        }
+    }
+}
