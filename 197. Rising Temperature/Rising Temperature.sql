b665c540acad6c63d8d3c60ea255aa76a9c84527
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-08-06 00:38:10 +0600

    added solution for Rising Temperature

---
+++ 197. Rising Temperature/Rising Temperature.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT Weather.id AS id
+FROM Weather
+JOIN Weather w ON DATEDIFF(Weather.recordDate, w.recordDate) = 1
+AND (Weather.temperature > w.temperature)
